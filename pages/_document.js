1c3286751c026a89a357c9f2a970f4732190ffa3
Startpee <86379169+startpee@users.noreply.github.com>
2022-10-26 09:35:06 -0300

    Commit from TeleportHQ

--- pages/_document.js
+++ pages/_document.js
@@ -40,6 +40,13 @@ class CustomDocument extends Document {
         <body>
           <Main></Main>
           <NextScript></NextScript>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html:
+                "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n        })(window,document,'script','dataLayer',' GTM-W5TLPCK');",
+            }}
+          ></script>
           <div
             dangerouslySetInnerHTML={{
               __html:
